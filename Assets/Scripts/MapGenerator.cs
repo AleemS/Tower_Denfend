85526a44c96d0474095e9dd39cd6af6ab7d0c033
Aleem Clyburn <aleemsayles@gmail.com>
2021-04-14 10:34:02 -0400

    update

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,19 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
+
+/*
+ * open a textfile in unity get result string
+ * based on the character of the string instanttly generate different kind of object
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
 public class MapGenerator : MonoBehaviour {
 
     public Transform TilePreFab;
+    public Transform Home;
+    public Transform End;
+    public Transform Spawn;
+    public Transform TilePreFab5;
     public Vector2 mapSize;
+    public Transform myTextFile;
+
+    public class ReadFromFile : MonoBehaviour
+    {
+        public string allText;
+    }
+
+    public void ParseFile()
+    {
+
+
+        string text = File.ReadAllText("level_" + "1" + ".txt");
+
+        char[] separators = { ','};
+        string[] seperators = { "/n" };
+        string[] strValues = text.Split(separators);
+
+        List<int> intValues = new List<int>();
+
+        foreach (string str in strValues)
+        {
+            int val = 0;
+            if (int.TryParse(str, out val))
+                intValues.Add(val);
+
+           
+        }
+    }
 
 
     [Range(0, 1)]
     public float outlinePercent;
-
+    
     public void GenerateMap()
     {
-        for(int x = 0; x < mapSize.x; x++)
+        for(int x = intValues; x < mapSize.x; x++)
         {
             for (int y = 0; y < mapSize.y; y++)
             {
